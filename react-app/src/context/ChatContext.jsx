e9e5e23af295f0f98c9cde120b5a4e1dc26f6569
Admin <diviyaprakash32@gmail.com>
2026-03-23 12:41:44 +0530

    23-03-2026

--- react-app/src/context/ChatContext.jsx
+++ react-app/src/context/ChatContext.jsx
@@ -8,7 +8,7 @@ const ChatContext = createContext();
 
 export const useChat = () => useContext(ChatContext);
 
-export const BACKEND_URL = 'http://localhost:9090';
+export const BACKEND_URL = import.meta.env.VITE_API_URL || 'http://localhost:9090';
 
 const compressAudio = async (dataUrl) => {
     // Basic stub for audio compression, returns same dataUrl for now
